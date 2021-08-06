dfe065b5aa25e2577c032dbe6b15f51949371023
ashley huang <asashu81@hotmail.com>
2021-08-06 12:45:52 +0800

    modify slider

--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
 let current = 0;
 const designs = document.querySelectorAll(".product-slider__design");
 const sliderBtn = document.querySelector(".navigator");
-const rbs = document.querySelectorAll('input[name="control"]');
 
 function handleNext() {
-  designs[current].style.transform = "translateX 100%";
-  designs[current >= designs.length - 1 ? 0 : current + 1].style.animation =
-    "showRight 1.5s forwards";
+  console.log("current:", current);
+  document.getElementById(`control-${current + 1}`).checked = true;
   if (current < designs.length - 1) {
     current++;
   } else {
     current = 0;
   }
-  console.log(current);
-  console.log(designs[current]);
 }
 
 function handlePrev() {
-  designs[current].style.animation = "hideLeft 1.5s forwards";
-  designs[current > 0 ? current - 1 : 2].style.animation =
-    "showLeft 1.5s forwards";
+  console.log("current:", current);
+
   if (current > 0) {
     current--;
   } else {
     current = designs.length - 1;
   }
-  console.log(current);
-  console.log("left current:", designs[current]);
+  document.getElementById(`control-${current + 1}`).checked = true;
 }
 
 sliderBtn.addEventListener("click", (e) => {
